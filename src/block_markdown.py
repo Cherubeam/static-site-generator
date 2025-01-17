3f1cdfb856d40acd19017fd49576afa9cc273048
Cherubeam <17480141+Cherubeam@users.noreply.github.com.>
2025-01-17 20:20:28 +0100

    add block_markdown.py

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -7,3 +7,17 @@ def markdown_to_blocks(markdown):
       block_strings.append(block.strip())
   
   return block_strings
+
+def block_to_block_type(markdown):
+  if markdown.startswith("#"):
+    return "heading"
+  elif markdown.startswith("```") and markdown.endswith("```"):
+    return "code"
+  elif markdown.startswith(">"):
+    return "quote"
+  elif markdown.startswith("* ") or markdown.startswith("- "):
+    return "unordered_list"
+  elif markdown[0].isdigit() and markdown[1:3] == ". ":
+    return "ordered_list"
+  else:
+    return "paragraph"
