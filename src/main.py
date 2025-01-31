08cb77b1b479c423ae88c149004d59bd1d4703d0
Cherubeam <17480141+Cherubeam@users.noreply.github.com.>
2025-01-31 17:02:56 +0100

    adapt copy_static_to_public function

--- src/main.py
+++ src/main.py
@@ -10,7 +10,12 @@ def main():
 def copy_static_to_public():
   if os.path.exists("./static") and os.path.exists("./public"):
     print(f"PATH EXISTS")
-    print(os.listdir("./static"))
+    
+    public_directory = os.listdir("./public")
+    if len(public_directory) == 0:
+      print("PUBLIC DIRECTORY IS EMPTY!")
+    else:
+      print(f"PUBLIC DIRECTORY CONTAINS FILES:\n{public_directory}")
   else:
     print(f"PATH DOES NOT EXISTS")
 
