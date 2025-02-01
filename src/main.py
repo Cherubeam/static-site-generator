d16d915466ab99c7275bbb3cfd382972bbdb8191
Cherubeam <17480141+Cherubeam@users.noreply.github.com.>
2025-02-01 14:13:55 +0100

    add delete and re-create of public folder

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import shutil
 import os
 
 from textnode import TextNode
@@ -7,16 +8,23 @@ def main():
   print(node)
   copy_static_to_public()
 
+
 def copy_static_to_public():
-  if os.path.exists("./static") and os.path.exists("./public"):
-    print(f"PATH EXISTS")
-    
-    public_directory = os.listdir("./public")
-    if len(public_directory) == 0:
-      print("PUBLIC DIRECTORY IS EMPTY!")
-    else:
-      print(f"PUBLIC DIRECTORY CONTAINS FILES:\n{public_directory}")
-  else:
-    print(f"PATH DOES NOT EXISTS")
+  static_dir = "./static"
+  public_dir = "./public"
+
+  # Create public directory if it does not exist
+  if not os.path.exists(public_dir):
+    os.mkdir(public_dir)
+
+  # Delete public directory if it is not empty
+  content_public_dir = os.listdir(public_dir)
+  if len(content_public_dir) != 0:
+    print(f"PUBLIC DIRECTORY CONTAINS FILES:\n{content_public_dir}")
+    shutil.rmtree(public_dir)
+
+  # try:
+  #   shutil.copy(static_dir, public_dir)
+
 
 main()
