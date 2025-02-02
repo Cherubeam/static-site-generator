b1dacc16e675ae443b5764f411d5bebdda567751
Cherubeam <17480141+Cherubeam@users.noreply.github.com.>
2025-02-02 17:52:09 +0100

    experiment with create and delete public directory

--- src/main.py
+++ src/main.py
@@ -22,6 +22,22 @@ def copy_static_to_public():
   if len(content_public_dir) != 0:
     print(f"PUBLIC DIRECTORY CONTAINS FILES:\n{content_public_dir}")
     shutil.rmtree(public_dir)
+    #print(f"PUBLIC DIRECTORY DELETED AND RE-CREATED")
+
+  # Copy files from static directory to public directory
+  content_static_dir = os.listdir(static_dir)
+  if len(content_static_dir) != 0:
+    print(f"STATIC DIRECTORY CONTAINS FILES:\n{content_static_dir}") 
+  
+    #path = os.path.join(public_dir, content_static_dir[0])
+    #print(f"PATH:\n{path}")
+    path = os.path.join(static_dir, content_static_dir[0])
+    shutil.copy(path, public_dir)
+
+  # For each listed item in content_static_dir check if it is a file or a directory
+  ## If it is a file copy it in the current directory level
+  ## Else, enter directory
+
 
   # try:
   #   shutil.copy(static_dir, public_dir)
