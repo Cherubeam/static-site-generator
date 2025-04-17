b733f15d1eec4fba63299e382e3ee00460671e05
Cherubeam <17480141+Cherubeam@users.noreply.github.com.>
2025-04-17 12:59:07 +0200

    fix bug in main.py

--- src/main.py
+++ src/main.py
@@ -5,9 +5,7 @@ import sys
 from copystatic import copy_files_recursive
 from gencontent import generate_page, generate_pages_recursive
 
-if not sys.argv[0]:
-    basepath = "/"
-basepath = sys.argv[0]
+default_basepath = "/"
 dir_path_static = "./static"
 dir_path_public = "./docs"
 dir_path_content = "./content"
@@ -15,6 +13,9 @@ template_path = "./template.html"
 
 
 def main():
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
@@ -23,11 +24,6 @@ def main():
     copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating page...")
-    # generate_page(
-    #     os.path.join(dir_path_content, "index.md"),
-    #     template_path,
-    #     os.path.join(dir_path_public, "index.html"),
-    # )
     generate_pages_recursive(
         basepath,
         dir_path_content,
